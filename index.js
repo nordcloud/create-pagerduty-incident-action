3cada12e0c61345f916d3faa881cf2ddbf0c05fe
Maciej Wilczyński <maciej.wilczynski@nordcloud.com>
2021-05-28 14:22:31 +0200

    Remove null coalescing operator

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const main = async () => {
     try {
         const githubContext = github.context.payload;
         const action = core.getInput('action');
-        const timestamp = core.getInput('timestamp') ?? new Date().toISOString();
-        const payload = core.getInput('custom_details') ?? {};
+        const timestamp = core.getInput('timestamp') || new Date().toISOString();
+        const payload = core.getInput('custom_details') || {};
 
         const response = await pd.event({
             data: {
                 routing_key: core.getInput('integration_key'),
                 event_action: action,
-                dedup_key: core.getInput('dedup_key') ?? undefined,
+                dedup_key: core.getInput('dedup_key') || undefined,
                 payload: action != 'trigger' ? undefined : {
                     summary: core.getInput('summary'),
                     source: core.getInput('source'),
                     severity: core.getInput('severity'),
                     timestamp: timestamp,
-                    component: core.getInput('component') ?? undefined,
-                    group: core.getInput('group') ?? undefined,
-                    class: core.getInput('class') ?? undefined,
+                    component: core.getInput('component') || undefined,
+                    group: core.getInput('group') || undefined,
+                    class: core.getInput('class') || undefined,
                     custom_details: {
                         github_context: githubContext,
                         ...payload
@@ -35,6 +35,7 @@ const main = async () => {
         core.setOutput('data', response.data);
 
         console.log('ts', timestamp);
+        console.log('pl', payload);
         console.log('Events API response status', response.status);
         console.log('Events API response data', response.data);
 
