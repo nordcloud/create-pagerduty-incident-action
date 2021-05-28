c869e6f463cb1886994664b006a7a5882abc399c
Maciej Wilczyński <maciej.wilczynski@nordcloud.com>
2021-05-28 14:27:33 +0200

    Compatibility fixes (#2)
    * Print node version
    
    * Comment out the code
    
    * Comment out the code
    
    * Test dist output
    
    * Change entrypoint
    
    * Update outputs
    
    * Debug ts output
    
    * Fix timestamp generation
    
    * Fix timestamp generation
    
    * Remove null coalescing operator
    
    * make gh context optional

--- index.js
+++ index.js
@@ -6,36 +6,44 @@ const main = async () => {
     try {
         const githubContext = github.context.payload;
         const action = core.getInput('action');
-        const timestamp = core.getInput('timestamp') ?? Date.now().toISOString();
-        const payload = core.getInput('custom_details') ?? {};
+        const timestamp = core.getInput('timestamp') || new Date().toISOString();
+        const payload = core.getInput('custom_details') || {};
 
         const response = await pd.event({
             data: {
                 routing_key: core.getInput('integration_key'),
                 event_action: action,
-                dedup_key: core.getInput('dedup_key') ?? undefined,
+                dedup_key: core.getInput('dedup_key') || undefined,
                 payload: action != 'trigger' ? undefined : {
                     summary: core.getInput('summary'),
                     source: core.getInput('source'),
                     severity: core.getInput('severity'),
                     timestamp: timestamp,
-                    component: core.getInput('component') ?? undefined,
-                    group: core.getInput('group') ?? undefined,
-                    class: core.getInput('class') ?? undefined,
+                    component: core.getInput('component') || undefined,
+                    group: core.getInput('group') || undefined,
+                    class: core.getInput('class') || undefined,
                     custom_details: {
-                        github_context: githubContext,
+                        github_context: core.getInput('include_github_context') == 'true' ? githubContext : undefined,
                         ...payload
                     }
                 }
             }
         });
 
-        core.setOutput('status', response.status);
-        core.setOutput('message', response.message);
+        core.setOutput('status', response.data.status);
+        core.setOutput('message', response.data.message);
+        core.setOutput('data', response.data);
 
-        console.log('Events API response', response);
+        console.log('Events API response status', response.status);
+        console.log('Events API response data', response.data);
+
+        if (response.status >= 400) {
+            core.setFailed(JSON.stringify(response.data));
+            return;
+        }
     } catch (error) {
         core.setFailed(error.message);
+        return;
     }
 };
 
